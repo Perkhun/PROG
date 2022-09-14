47d5acbb535b92a7270cb58b613f4d84b56739dc
Perkhun <iryna.perkhun@gmail.com>
2022-09-14 14:10:16 +0300

    first commit

---
+++ bitcoin.js
@@ -0,0 +1,10 @@
+let price = 21000;
+console.log("Bitcoin price: " + price)
+
+let myMoney = 1000;
+console.log("You have: " + myMoney + " $")
+
+let bitcoin = myMoney / price;
+console.log("You can buy " + bitcoin + " Bitcoins")
+
+
